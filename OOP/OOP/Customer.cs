97f54bb23dd0101bb33adb2165e3eeff494eff18
isacsonsyster <ester.isacson@elev.ga.ntig.se>
2020-10-06 12:41:45 +0200

    OOP

---
+++ OOP/OOP/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class Customer
+    {
+        public List<Product> _cart = new List<Product>();
+
+        public void Shopping()
+        {
+            Console.WriteLine("(1) Pink \n (2) Yellow \n (3) Purple \n (4) Cart List \n (5) Quit Shopping");
+        }
+
+    }
+}
---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer Ester = new Customer();
+
+            bool shop = true;
+            while (shop)
+            {
+                Ester.Shopping();
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Product pink = new Product();
+                        pink._productName = "pink";
+                        Ester._cart.Add(pink);
+                        break;
+                    case 2:
+                        Product yellow = new Product();
+                        yellow._productName = "yellow";
+                        Ester._cart.Add(yellow);
+                        break;
+                    case 3:
+                        Product purple = new Product();
+                        purple._productName = "purple";
+                        Ester._cart.Add(purple);
+                        break;
+                    case 4:
+                        foreach(Product item in Ester._cart)
+                        {
+                            Console.WriteLine(item._productName);
+                        }
+                        break;
+                    case 5:
+                        shop = false;
+                        break;
+                }
+            }
+        }
+    }
+}
